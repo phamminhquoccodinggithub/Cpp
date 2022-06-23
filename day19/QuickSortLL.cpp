e128d8fa06f4217a7ba8ef520a13da387ecbfa0d
phamminhquoccodinggithub <>
2022-06-23 12:01:20 +0700

    quicksortLL

--- day19/QuickSortLL.cpp
+++ day19/QuickSortLL.cpp
@@ -3,7 +3,7 @@ using namespace std;
 struct Node
 {
     int data;
-     Node *next;
+    Node *next;
 };
 void push(Node **headRef, int data)
 {
@@ -21,31 +21,33 @@ void print(Node *head)
     }
     cout << endl;
 }
- Node* getTail( Node* cur)
+Node *getTail(Node *cur)
 {
     while (cur != NULL && cur->next != NULL)
         cur = cur->next;
     return cur;
 }
- 
+
 // Partitions the list taking the last element as the pivot
- Node* partition( Node* head,  Node* end,
-                        Node** newHead,
-                        Node** newEnd)
+Node *partition(Node *head, Node *end,
+                Node **newHead,
+                Node **newEnd)
 {
-     Node* pivot = end;
-     Node *prev = NULL, *cur = head, *tail = pivot;
- 
+    Node *pivot = end;
+    Node *prev = NULL, *cur = head, *tail = pivot;
+
     // During partition, both the head and end of the list
     // might change which is updated in the newHead and
     // newEnd variables
-    while (cur != pivot) {
-        if (cur->data < pivot->data) {
+    while (cur != pivot)
+    {
+        if (cur->data < pivot->data)
+        {
             // First node that has a value less than the
             // pivot - becomes the new head
             if ((*newHead) == NULL)
                 (*newHead) = cur;
- 
+
             prev = cur;
             cur = cur->next;
         }
@@ -55,71 +57,70 @@ void print(Node *head)
             // tail
             if (prev)
                 prev->next = cur->next;
-             Node* tmp = cur->next;
+            Node *tmp = cur->next;
             cur->next = NULL;
             tail->next = cur;
             tail = cur;
             cur = tmp;
         }
     }
- 
+
     // If the pivot data is the smallest element in the
     // current list, pivot becomes the head
     if ((*newHead) == NULL)
         (*newHead) = pivot;
- 
+
     // Update newEnd to the current last node
     (*newEnd) = tail;
- 
+
     // Return the pivot node
     return pivot;
 }
- 
+
 // here the sorting happens exclusive of the end node
- Node* quickSortRecur( Node* head,
-                             Node* end)
+Node *quickSortRecur(Node *head,
+                     Node *end)
 {
     // base condition
     if (!head || head == end)
         return head;
- 
+
     Node *newHead = NULL, *newEnd = NULL;
- 
+
     // Partition the list, newHead and newEnd will be
     // updated by the partition function
-     Node* pivot
-        = partition(head, end, &newHead, &newEnd);
- 
+    Node *pivot = partition(head, end, &newHead, &newEnd);
+
     // If pivot is the smallest element - no need to recur
     // for the left part.
-    if (newHead != pivot) {
+    if (newHead != pivot)
+    {
         // Set the node before the pivot node as NULL
-         Node* tmp = newHead;
+        Node *tmp = newHead;
         while (tmp->next != pivot)
             tmp = tmp->next;
         tmp->next = NULL;
- 
+
         // Recur for the list before pivot
         newHead = quickSortRecur(newHead, tmp);
- 
+
         // Change next of last node of the left half to
         // pivot
         tmp = getTail(newHead);
         tmp->next = pivot;
     }
- 
+
     // Recur for the list after the pivot element
     pivot->next = quickSortRecur(pivot->next, newEnd);
- 
+
     return newHead;
 }
- 
+
 // The main function for quick sort. This is a wrapper over
 // recursive function quickSortRecur()
-void quickSort( Node** headRef)
+void quickSort(Node **headRef)
 {
-    (*headRef)
-        = quickSortRecur(*headRef, getTail(*headRef));
+    (*headRef) = quickSortRecur(*headRef, getTail(*headRef));
     return;
 }
 int main()
@@ -131,6 +132,8 @@ int main()
     push(&node, 5);
     push(&node, 7);
     push(&node, 9);
+    print(node);
+    quickSort(&node);
     print(node);
     return 0;
 }
