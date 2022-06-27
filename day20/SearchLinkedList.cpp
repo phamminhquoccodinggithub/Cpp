8ce863ea05af9d4174a2a029f2b7be8e2edb5c83
phamminhquoccodinggithub <74046383+phamminhquoccodinggithub@users.noreply.github.com>
2022-06-27 11:57:19 +0700

    Update SearchLinkedList.cpp

--- day20/SearchLinkedList.cpp
+++ day20/SearchLinkedList.cpp
@@ -60,6 +60,38 @@ struct Node *middle(Node *first, Node *last)
     }
     return cur;
 }
+struct Node* binarySearch(Node *head, int value)
+{
+    struct Node* start = head;
+    struct Node* last = NULL;
+  
+    do
+    {
+        // Find middle
+        Node* mid = middle(start, last);
+  
+        // If middle is empty
+        if (mid == NULL)
+            return NULL;
+  
+        // If value is present at middle
+        if (mid -> data == value)
+            return mid;
+  
+        // If value is more than mid
+        else if (mid -> data < value)
+            start = mid -> next;
+  
+        // If the value is less than mid.
+        else
+            last = mid;
+  
+    } while (last == NULL ||
+             last != start);
+  
+    // value not present
+    return NULL;
+}
 int main()
 {
     Node *node;
