1757729ba7b3b0d1e0f61ba753cf32a21169f2c7
phamminhquoccodinggithub <74046383+phamminhquoccodinggithub@users.noreply.github.com>
2022-06-29 09:59:47 +0700

    Create rdState.cpp

---
+++ Review/rdState.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+void checkstatus(ifstream &in);
+int main(int argc, char *argv[])
+{
+if(argc!=2) {
+cout << "Usage: Display <filename>\n";
+return 1;
+}
+ifstream in(argv[1]);
+if(!in) {
+cout << "Cannot open input file.\n";
+return 1;
+}
+char c;
+while(in.get(c)) {
+if(in) cout << c;
+checkstatus(in);
+}
+checkstatus(in); // check final status
+in.close();
+return 0;
+}
+void checkstatus(ifstream &in)
+{
+ios::iostate i;
+i = in.rdstate();
+if(i & ios::eofbit)
+cout << "EOF encountered\n";
+
+else if(i & ios::failbit)
+cout << "Non-Fatal I/O error\n";
+else if(i & ios::badbit)
+cout << "Fatal I/O error\n";
+}
