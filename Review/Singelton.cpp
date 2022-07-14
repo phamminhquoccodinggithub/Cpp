948b68861600e753de36ae058722080f9f4c4131
phamminhquoccodinggithub <74046383+phamminhquoccodinggithub@users.noreply.github.com>
2022-07-14 20:46:06 +0700

    Create Singelton.cpp

---
+++ Review/Singelton.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+enum vehicletype {
+	twoWheeler, threewheeler, fourwheeler
+};
+
+class vehicle {
+public:
+	virtual void printvehicle() = 0;
+	static vehicle* create(VehicleType type);
+	virtual ~vehicle(){}
+};
+class Twowheeler : public vehicle {
+public:
+	void printvehicle() {
+		cout << "I am two wheeler" << endl;
+	}
+};
+class Threewheeler : public vehicle {
+public:
+	void printvehicle() {
+		cout << "I am three wheeler" << endl;
+	}
+};
+class Fourwheeler : public vehicle {
+	public:
+	void printvehicle() {
+		cout << "I am four wheeler" << endl;
+	}
+};
+
+// Factory method to create objects of different types.
+// Change is required only in this function to create a new object type
+vehicle* vehicle::create(vehicletype type) {
+	if (type == twowheeler)
+		return new Twowheeler();
+	else if (type == threewheeler)
+		return new Threewheeler();
+	else if (type == fourwheeler)
+		return new Fourwheeler();
+	else return NULL;
+}
+
+// Client class
+class client {
+public:
+
+	// Client doesn't explicitly create objects
+	// but passes type to factory method "create()"
+	client()
+	{
+		vehicletype type = threewheeler;
+		pvehicle = vehicle::create(type);
+	}
+	~client() {
+		if (pvehicle) {
+			delete pvehicle;
+			pvehicle = NULL;
+		}
+	}
+	vehicle* getvehicle() {
+		return pvehicle;
+	}
+
+private:
+	vehicle *pvehicle;
+};
+
+
+int main() {
+	client *pclient = new client();
+	vehicle * pvehicle = pclient->getvehicle();
+	pvehicle->printvehicle();
+	delete pclient;
+	return 0;
+}
